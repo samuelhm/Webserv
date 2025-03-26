5a7f384060d117e23423ef52e0ec05a4b0e93c42
Samuel Hurtado Marin <shurtado@student.42barcelona.com>
2025-03-26 13:42:49 +0100

    Insert listen port logic to server object

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 11:44:20 by shurtado          #+#    #+#             */
-/*   Updated: 2025/03/25 18:47:18 by shurtado         ###   ########.fr       */
+/*   Updated: 2025/03/26 13:37:25 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,61 +19,50 @@
 #include <string.h> // memset
 #include <iostream> // cout, endl
 #include <sys/epoll.h> // epoll
+#include <stdio.h>
 
 int main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	Server server;
-// inicializamos socket
-	int serverFd = socket(AF_INET, SOCK_STREAM, 0);
-	int yes = 1;
-// Poder reutilizar el mismo puerto sin tener que esperar en caso de fallo del programa
-	setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-	fcntl(serverFd, F_SETFL, O_NONBLOCK);
-// carga el host name y port en el addrinfo
-// hints -> asignamos opciones de config
-	struct addrinfo hints, *res;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;			// IPv4
-	hints.ai_socktype = SOCK_STREAM;	// TCP
-	hints.ai_flags = AI_PASSIVE;		// Para bind() (escuchar)
-// con las opciones dadas en hints configura res
-	int err = getaddrinfo(server.getHostName().c_str(), server.getPort().c_str(), &hints, &res);
-	if (err)
-	{
-		std::cout << "error: getaddrinfo" << std::endl << gai_strerror(err) << std::endl;
-		return (1);
-	}
-// vincula fd del servidor al host name y port
-	else if (bind(serverFd, res->ai_addr, res->ai_addrlen) == -1) {
-		std::cout << "error: bind" << std::endl;
-		return (1);
-	}
-// habilitamos el fd para que se quede escuchando
-	listen(serverFd, SOMAXCONN);
+	std::vector<Server*> Servers;
+	Servers.push_back(new Server("servidor1", "8080"));
+	// Servers.push_back(new Server("servidor2", "8181"));
+
+
 // creamos el pool (epoll) de eventos
 	int pollFd = epoll_create(1);
 	if (pollFd == -1) {
 		std::cout << "error: epoll_create1" << std::endl;
 		return (1);
 	}
-// vincula fd del pool de eventos al host name y port
+
 	struct epoll_event ev;
-	ev.events = EPOLLIN;
-	ev.data.fd = serverFd;
-	if (epoll_ctl(pollFd, EPOLL_CTL_ADD, serverFd, &ev) == -1) {
-		std::cout << "error: epoll_create1" << std::endl;
-		return 1;
+
+	for (std::vector<Server*>::iterator it = Servers.begin(); it != Servers.end(); ++it)
+	{
+		ev.events = EPOLLIN;
+		ev.data.fd = (*it)->getServerFd();
+		if (epoll_ctl(pollFd, EPOLL_CTL_ADD, (*it)->getServerFd(), &ev) == -1) {
+			perror("epoll_ctl");
+			return 1;
+		}
+
 	}
+
+	int nfds;
 // esperamos a que se acceda al ej. localhost:8080
 	struct epoll_event events[1024]; // tamaño = número máximo de eventos a esperar
-	int nfds = epoll_wait(pollFd, events, 1024, -1); // -1 = bloquea indefinidamente
-	if (nfds == -1) {
-		std::cout << "error: epoll_wait" << std::endl;
-		return 1;
+	while (1)
+	{
+		nfds = epoll_wait(pollFd, events, 1024, -1); // -1 = bloquea indefinidamente
+		if (nfds == -1) {
+			std::cout << "error: epoll_wait" << std::endl;
+			return 1;
+		}
+		// std::cout << " Server FD: "<< serve << "\nPollFD: "<< pollFd << "\nnfds: "<< nfds << "\nstdout: "<< STDOUT_FILENO << std::endl;
 	}
-	close(serverFd);
-	freeaddrinfo(res);
+
+
 	return 0;
 }
