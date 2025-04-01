80f5eff068e62e6355435ca7f3b78181b5a49ef0
erikgonk <twire01@gmail.com>
2025-04-01 14:51:30 +0200

    [_header & our ft_substr and print_map added] idk what's going on with the map

--- src/HTTP/HttpRequest.cpp
+++ src/HTTP/HttpRequest.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HttpRequest.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
+/*   By: erigonza <erigonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 13:11:54 by shurtado          #+#    #+#             */
-/*   Updated: 2025/03/30 17:22:18 by shurtado         ###   ########.fr       */
+/*   Updated: 2025/04/01 14:49:33 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,40 @@
 #include <exception>
 #include <iostream>
 
+template <typename K, typename V>
+void print_map(const std::map<K, V>& m) {
+    // Use an iterator to loop through the map
+    typename std::map<K, V>::const_iterator it;
+    
+    // Iterate through the map and print each key-value pair
+    for (it = m.begin(); it != m.end(); ++it) {
+        std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
+    }
+}
+
+std::string ft_substr(const std::string& s, unsigned int start, size_t len)
+{
+    // If start is beyond the string length, return empty string
+    if (start >= s.length())
+        return "";
+
+    // Calculate the effective length - don't read past end of string
+    size_t effective_len = std::min(len, s.length() - start);
+    
+    // Return the substring using std::string's substr method
+    return s.substr(start, effective_len);
+}
+
 const str HttpRequest::saveHeader(const str &request) {
 	if (request.find(": ") == str::npos) {
 		return request;
 	}
 	int start = request.find(": ");
 	int end = request.find("\r\n");
-	_header[request.substr(0, start)] = request.substr(start, end);
-	saveHeader(request.substr(start + end));
-	return request;
+	_header[ft_substr(request, 0, start)] = ft_substr(request, start + 2, end);
+	saveHeader(ft_substr(request, start + 2, end - start - 2));
+	// std::cout << request.substr(0, start) << "---" << request.substr(start + end + 2);
+	return request;	
 }
 
 void HttpRequest::checkHeaderMRP(const str &line) {
@@ -67,6 +92,7 @@ HttpRequest::HttpRequest(str request) : AHttp(request), _badRequest(false) {
 	try {
 		checkHeaderMRP(line);
 		saveHeader(request.substr(end));
+		print_map(_header);
 	} catch(const badHeaderException &e) {
 		std::cout << e.what() << std::endl;
 		_badRequest = true;
