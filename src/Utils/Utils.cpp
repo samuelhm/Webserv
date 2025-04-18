6e3d09e25b04ad7b096f1206619eb4add1ae690d
Samuel Hurtado <lnx.wifi@gmail.com>
2025-04-18 22:48:39 +0200

    Implemented isDirectory

--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -253,3 +253,12 @@ str Utils::getMimeType(const str &filename)
 	}
 	return "application/octet-stream";
 }
+
+bool isDirectory(const std::string &path) {
+	DIR *dir = opendir(path.c_str());
+	if (dir) {
+		closedir(dir);
+		return true;
+	}
+	return false;
+}
