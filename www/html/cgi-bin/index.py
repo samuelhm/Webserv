59861dc683c83491f04c98781c091b10116c1c35
erikgonk <twire01@gmail.com>
2025-04-18 15:08:16 +0200

    correction

---
+++ www/html/cgi-bin/index.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import cgi
+import os
+import html
+
+# Analiza los parámetros del query string
+params = cgi.FieldStorage()
+nombre = params.getfirst("nombre", "Mundo")
+nombre = html.escape(nombre)  # Sanitiza para evitar inyección de HTML
+
+print("Content-Type: text/html\n")
+print(f"""
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Hola</title>
+</head>
+<body>
+    <h1>Hola {nombre}</h1>
+</body>
+</html>
+""")
